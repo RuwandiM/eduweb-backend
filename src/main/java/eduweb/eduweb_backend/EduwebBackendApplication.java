713d08d74a7f4657efd22e68960fc5d218d11fa6
M.R.MAYUNIKA <RuwandiMayunika01@gmail.com>
2025-05-29 16:44:09 +0530

    initiate the project

---
+++ src/main/java/eduweb/eduweb_backend/EduwebBackendApplication.java
@@ -0,0 +1,13 @@
+package eduweb.eduweb_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduwebBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduwebBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eduweb/eduweb_backend/EduwebBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package eduweb.eduweb_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduwebBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
